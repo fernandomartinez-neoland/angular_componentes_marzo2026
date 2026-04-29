a6cf892d601db59df0da491c96e792cbbe9a6336
altrios <famartinez.ti@gmail.com>
2026-04-29 10:07:01 +0200

    feat: implement initial project structure with Header and Formulario components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
